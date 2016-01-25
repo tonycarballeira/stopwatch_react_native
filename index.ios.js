2f55cc854bdb12290d148355aa6444b0bf9dbbe9
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-25 17:17:28 -0500

    Created a start and lap view component

--- index.ios.js
+++ index.ios.js
@@ -6,12 +6,34 @@ var {
   AppRegistry
 } = React;
 
-// The above syntax is the same as writing out (EX: var Text = React.Text;) for each library property.
+// The above syntax is the same as writing out (EX: var Text = React.Text;) for each library property. this is called variable destructuring.
 
 
 var StopWatch = React.createClass({
   render: function() {
+    return <View>
 
+      <Text>
+        00:00.00
+      </Text>
+
+    </View>
+  },
+
+  startStopButton: function() {
+    return <View>
+        <Text>
+          Start
+        </Text>
+      </View>
+  },
+
+  lapButton: function() {
+    return <View>
+        <Text>
+          Lap
+        </Text>
+      </View>
   }
 }); 
 
