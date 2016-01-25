65a3eebb71af6a3c92623877f57258c8de8e6cad
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-25 16:54:16 -0500

    Using ES2015 syntax to resister StopWatch component in Index

--- index.ios.js
+++ index.ios.js
@@ -6,4 +6,20 @@ var {
   AppRegistry
 } = React;
 
-// The above syntax is the same as writing out (EX: var Text = React.Text;) for each library property. 
+// The above syntax is the same as writing out (EX: var Text = React.Text;) for each library property.
+
+
+var StopWatch = React.createClass({
+  render: function() {
+
+  }
+}); 
+
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
+
+// The above code is the same as writing out the following:
+
+// AppRegistry.registerComponent('stopwatch', function(){
+//   return StopWatch;
+// })
